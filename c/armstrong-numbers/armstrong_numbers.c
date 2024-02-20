c05508c6e901f6a581afa90aa4537d0830638ef2
Nerton <thiago2nerton@gmail.com>
2024-02-20 10:57:55 -0300

    A

---
+++ c/armstrong-numbers/armstrong_numbers.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+#include "armstrong_numbers.h"
+
+bool is_armstrong_number(){
+        int number;
+	scanf("%i",&number);
+    int count=0;
+	int numberv= number;
+    int var = number;
+    //conta as casas decimais
+    while(var >0){     
+    	count++;
+        var = var/10;   
+    }
+// caso de uma casa decimal
+    if (count == 1)
+        {
+        return true;
+        }
+    
+    int vetor[count], eq=0;
+    
+    for(int i =0; i<count;i++){
+        vetor[i] = numberv%10;
+	    numberv = numberv/10;
+	    eq = eq + pow(vetor[i],count); 
+	}
+    
+	if(eq == number){return true;}
+    else{return false;}
+
+}
+
+
---
+++ c/armstrong-numbers/armstrong_numbers.h
@@ -0,0 +1,8 @@
+#ifndef ARMSTRONG_NUMBERS_H
+#define ARMSTRONG_NUMBERS_H
+
+#include <stdbool.h>
+#include <stdio.h>
+bool is_armstrong_number();
+
+#endif
