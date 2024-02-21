ba69a34a9107de1886b6e30df8e27bf582258f5e
Nerton <thiago2nerton@gmail.com>
2024-02-21 19:51:15 -0300

    Solving

--- c/armstrong-numbers/armstrong_numbers.c
+++ c/armstrong-numbers/armstrong_numbers.c
@@ -2,29 +2,26 @@
 #include <math.h>
 #include "armstrong_numbers.h"
 
-bool is_armstrong_number(){
-        int number;
-	scanf("%i",&number);
-    int count=0;
-	int numberv= number;
+bool is_armstrong_number(int number){
+    int count = 0, temp = number;
     int var = number;
     //conta as casas decimais
-    while(var >0){     
-    	count++;
-        var = var/10;   
+    while(var > 0){     
+        var = var / 10;
+        count++;   
     }
     // caso de uma casa decimal
     if (count == 1)
         {
         return true;
         }
-    int vetor[count], eq = 0;
-    for(int i =0; i<count;i++){
-        vetor[i] = numberv % 10;
-	    numberv = numberv / 10;
+    int vetor[count], eq = 0, i = 0;
+    while (temp > 0){
+        vetor[i] = temp % 10;
 	    eq = eq + pow(vetor[i], count); 
+        temp = temp / 10;
+        i++;
 	}
-    
 	return (eq == number);
 }
 
