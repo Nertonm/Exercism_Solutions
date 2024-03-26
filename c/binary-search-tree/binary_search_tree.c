6947643e4210667874614b182158c952657a0ad8
Nerton <thiago2nerton@gmail.com>
2024-03-25 23:06:23 -0300

    Continuing binary_search_tree

--- c/binary-search-tree/binary_search_tree.c
+++ c/binary-search-tree/binary_search_tree.c
@@ -1,16 +1,35 @@
 #include "binary_search_tree.h"
+#include "stdlib.h"
 
 node_t *build_tree(int *tree_data, size_t tree_data_len){
-    int tam =  sizeof(tree_data) / sizeof(tree_data[0]);
+    int tam = tree_data_len;
+    //int tam =  sizeof(*tree_data) / sizeof(tree_data[0]);
     if (tree_data){ 
         // Setting Up tree root
         node_t* tree = (node_t*)malloc(sizeof(node_t));
         tree->data = tree_data[0];
-        while (int i = 1; i < tam; i++){
-            if (tree->data > tree_data[i]){
-                node_t* treeEsq = (node_t*)malloc(sizeof(node_t));   
-            }
+        tree->left = NULL;
+        tree->right = NULL;
+        node_t* aux = tree;
+        for (int i = 1; i < tam; i++){
+            aux = tree;
+            if (aux->data > tree_data[i]){
+                while (aux->left) 
+                    aux = aux->left;
+                node_t* treeEsq = (node_t*)malloc(sizeof(node_t));
+                treeEsq->data = tree_data[i];
+                aux->left = treeEsq;
+            } else {
+                 if (aux->data < tree_data[i]){
+                    while (aux->right) 
+                        aux = aux->right;
+                    node_t* treeDir = (node_t*)malloc(sizeof(node_t));
+                    treeDir->data = tree_data[i];
+                    aux->right = treeDir;    
+                    }
+                }
         }
+        return tree;
     }
     return NULL;
 }
