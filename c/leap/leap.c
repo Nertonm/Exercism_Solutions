95e7b9486d3fd5279a106721505ce66dac96ef67
Nerton <thiago2nerton@gmail.com>
2024-04-26 03:14:58 -0300

    Solving Leap

--- c/leap/leap.c
+++ c/leap/leap.c
@@ -2,9 +2,11 @@
 
 bool leap_year(int year){
     if (!(year % 4)){
-        
-
+        if(!(year % 100))
+            if (year % 400) return false;
+        return true;
     }
+    return false;
 }
 
 
