afdb661dc29286195c0afe6c366693a66c609eaa
Nerton <thiago2nerton@gmail.com>
2024-04-28 03:46:26 -0300

    Starting

--- c/difference-of-squares/difference_of_squares.c
+++ c/difference-of-squares/difference_of_squares.c
@@ -1,1 +1,6 @@
 #include "difference_of_squares.h"
+
+unsigned int sum_of_squares(unsigned int number);
+unsigned int square_of_sum(unsigned int number);
+unsigned int difference_of_squares(unsigned int number);
+
