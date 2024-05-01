85546dc2b529c23f5d0347c247933687fde438f8
Nerton <thiago2nerton@gmail.com>
2024-04-30 21:29:12 -0300

    Still issues

--- c/difference-of-squares/difference_of_squares.c
+++ c/difference-of-squares/difference_of_squares.c
@@ -3,9 +3,19 @@
 unsigned int sum_of_squares(unsigned int number){
     unsigned int num = 0;
     for(unsigned int i = 1; i < number; i++)
-        num += i*i;
+        num = num + i*i;
     return num;
 }
-unsigned int square_of_sum(unsigned int number);
-unsigned int difference_of_squares(unsigned int number);
+unsigned int square_of_sum(unsigned int number)
+    unsigned int num = 0;
+    for(unsigned int i = 1; i < number; i++)
+        num += i;
+    num = num*num;
+    return num;
+}
+unsigned int difference_of_squares(unsigned int number){
+    unsigned int num1 = square_of_sum(number);
+    unsigned int num2 = sum_of_squares(number);
+    return num1 - num2;
+}
 
