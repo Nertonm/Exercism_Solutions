819242f41f7a121e06649c909eab06df22c71358
Nerton <thiago2nerton@gmail.com>
2024-05-01 19:49:35 -0300

    Still not solved

--- c/grains/grains.c
+++ c/grains/grains.c
@@ -1,7 +1,19 @@
 #include "grains.h"
 
 
-uint64_t square(uint8_t index);
-uint64_t total(void);
+uint64_t square(uint8_t index){
+    uint64_t num = 1;
+    if (!index) return 0;
+    for (uint64_t i = 2; i <= index; i++)
+        num = num * 2;   
+    return num;
+}
+
+uint64_t total(void){
+    uint64_t num = 1;
+    for (uint64_t i = 2; i <= 63; i++)
+        num = num * 2;   
+    return num;
+}
 
 
