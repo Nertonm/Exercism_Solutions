ef3f1edd1c34a4ec6f616eda3242663e2a628299
Nerton <thiago2nerton@gmail.com>
2024-05-06 22:55:29 -0300

    First try

--- c/collatz-conjecture/collatz_conjecture.c
+++ c/collatz-conjecture/collatz_conjecture.c
@@ -1,5 +1,13 @@
 #include "collatz_conjecture.h"
 
-int steps(int start);
+int steps(int start){
+    int ret = 0;
+    for (int i = 0; start > 1; i++){
+        if (start % 2)
+            start = (start * 3) + 1;
+        start = start / 2; 
+    }
+    return ret;
+}
 
 
