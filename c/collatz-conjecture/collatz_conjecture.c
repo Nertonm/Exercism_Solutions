02a1af0976e522cf993aa1581c76c613fcbf10cc
Nerton <thiago2nerton@gmail.com>
2024-05-06 23:00:47 -0300

    Second try

--- c/collatz-conjecture/collatz_conjecture.c
+++ c/collatz-conjecture/collatz_conjecture.c
@@ -2,7 +2,8 @@
 
 int steps(int start){
     int ret = 0;
-    for (int i = 0; start > 1; i++){
+    if (start < 1) return -1;
+    for (int i = 0; start != 1; i++){
         if (start % 2)
             start = (start * 3) + 1;
         start = start / 2; 
