32659524a0f215ff9f3e7b74285592fde4530271
Nerton <thiago2nerton@gmail.com>
2024-05-06 23:06:24 -0300

    Problem solved

--- c/collatz-conjecture/collatz_conjecture.c
+++ c/collatz-conjecture/collatz_conjecture.c
@@ -3,10 +3,11 @@
 int steps(int start){
     int ret = 0;
     if (start < 1) return -1;
-    for (int i = 0; start != 1; i++){
-        if (start % 2)
+    for (ret = 0; start != 1; ret++){
+        if (start % 2) 
             start = (start * 3) + 1;
-        start = start / 2; 
+        else
+            start = start / 2; 
     }
     return ret;
 }
