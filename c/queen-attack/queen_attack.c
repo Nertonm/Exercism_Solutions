469c490fafd0e1b8bb68ab966a597496c24c8129
Nerton <thiago2nerton@gmail.com>
2024-05-08 18:16:56 -0300

    Before start

--- c/queen-attack/queen_attack.c
+++ c/queen-attack/queen_attack.c
@@ -1,1 +1,12 @@
 #include "queen_attack.h"
+
+/*typedef enum { CAN_NOT_ATTACK, CAN_ATTACK, INVALID_POSITION } attack_status_t;
+
+typedef struct {
+   uint8_t row;
+   uint8_t column;
+} position_t;
+
+attack_status_t can_attack(position_t queen_1, position_t queen_2);
+*/
+
